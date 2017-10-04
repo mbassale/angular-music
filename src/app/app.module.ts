ee3824c12b29ce5c7b6d9a0f7a4b56622d8870e3
Marco Bassaletti <bassaletti@gmail.com>
2017-10-04 07:26:50 -0300

    Created skeleton components for album, artist, search and track. Also loaded angular router modules and defined initial routes.
    Signed-off-by: Marco Bassaletti <bassaletti@gmail.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,43 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {AlbumComponent} from './album/album.component';
+import {ArtistComponent} from './artist/artist.component';
+import {SearchComponent} from './search/search.component';
+import {TrackComponent} from './track/track.component';
+import {RouterModule, Routes} from '@angular/router';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
+import {APP_BASE_HREF, HashLocationStrategy, LocationStrategy} from '@angular/common';
+
+const routes: Routes = [
+  {path: '', redirectTo: 'search', pathMatch: 'full'},
+  {path: 'search', component: SearchComponent},
+  {path: 'artists/:id', component: ArtistComponent},
+  {path: 'tracks/:id', component: TrackComponent},
+  {path: 'albums/:id', component: AlbumComponent},
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AlbumComponent,
+    ArtistComponent,
+    SearchComponent,
+    TrackComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    RouterModule.forRoot(routes)
   ],
-  providers: [],
+  providers: [
+    {provide: APP_BASE_HREF, useValue: '/'},
+    {provide: LocationStrategy, useClass: HashLocationStrategy}
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
