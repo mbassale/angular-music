b23230a79af550f42ea96752f83399d5763d9674
Marco Bassaletti <bassaletti@gmail.com>
2017-10-04 09:00:27 -0300

    Bug-fix: spotifyApiKey doesn't exist on production environment file.
    Signed-off-by: Marco Bassaletti <bassaletti@gmail.com>

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  spotifyApiKey: 'BQDOVPV-v-vds4XhvQaLP2unL-6SFWTJQrNahRa5QSQWLuIOXLW_lO0VH6m3dCICIKYT6bBARY3bn-Fw47vO2rojrt3AqsKS_4k2IKG4y4W0_VEaKcUN0OyJNnAEQHL2MRUtinq-Pzy1mNJsx8xFj3aOTpYG_Yi_vyc'
 };
